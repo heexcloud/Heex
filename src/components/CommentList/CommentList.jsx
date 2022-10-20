abf134cc0e72b21a564ce95f8eefd8640e412712
Yumin Gui <yumindev@gmail.com>
2022-10-19 19:27:40 -0700

    fix: reply to some reply

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,9 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { query } from "../../utils";
 import { ACTION, useHeexContext } from "../../context";
+import { useMemoizedFn } from "../../hooks";
 
 export const CommentList = () => {
     const { state, dispatch } = useHeexContext();
+    const [replyEditor, setReplyEditor] = useState();
+
+    const toggleReplyEditor = useMemoizedFn((replyTo) => {
+        if (replyEditor?.cid === replyTo.cid) {
+            setReplyEditor();
+        } else {
+            setReplyEditor(replyTo);
+        }
+    });
+
     useEffect(() => {
         Promise.all([query.getCommentCount(), query.getComments()]).then(
             (res) => {
@@ -33,6 +44,19 @@ export const CommentList = () => {
                         <div>{comment.username}</div>
                         <div>{comment.comment}</div>
                         <div>{comment.createdAt}</div>
+                        <button
+                            onClick={() =>
+                                toggleReplyEditor({
+                                    cid: comment.objectId,
+                                    at: comment.username,
+                                })
+                            }
+                        >
+                            Reply
+                        </button>
+                        {replyEditor?.cid === comment.objectId && (
+                            <div>Reply editor</div>
+                        )}
                     </div>
                 );
             })}
