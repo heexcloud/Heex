8260ef5f2c1d43d4f1f95e847716db8264bb3c36
Yumin Gui <yumindev@gmail.com>
2022-10-22 12:26:50 -0700

    render CommentEditor

--- src/components/_CommentItem/CommentItem.jsx
+++ src/components/_CommentItem/CommentItem.jsx
@@ -4,6 +4,33 @@ import { CommentEditor } from "../CommentEditor";
 export const CommentItem = (props) => {
     const { comment, replyEditor, setReplyEditor, toggleReplyEditor } = props;
 
+    const renderCommentEditor = (thread, reply, replyToId) => {
+        if (replyEditor?.cid !== replyToId) return null;
+
+        // reply to thread only
+        if (thread && !reply) {
+            return (
+                <CommentEditor
+                    thread={thread}
+                    onSubmitSuccess={setReplyEditor}
+                />
+            );
+        }
+
+        // reply to thread's reply
+        if (thread && reply) {
+            return (
+                <CommentEditor
+                    thread={thread}
+                    reply={reply}
+                    onSubmitSuccess={setReplyEditor}
+                />
+            );
+        }
+
+        return null;
+    };
+
     return (
         <div className="heex-comment-list-item" key={comment.objectId}>
             <div className="heex-comment-thread-root">
@@ -21,22 +48,35 @@ export const CommentItem = (props) => {
                     Reply
                 </button>
 
-                {replyEditor?.cid === comment.objectId && (
-                    <CommentEditor
-                        thread={comment}
-                        onSubmitSuccess={setReplyEditor}
-                    />
-                )}
+                {renderCommentEditor(comment, null, comment.objectId)}
             </div>
             {comment.replies?.length > 0 && (
                 <div className="heex-comment-thread-reply">
                     {comment.replies.map((reply) => {
                         return (
-                            <CommentItem
-                                comment={reply}
-                                replyEditor={replyEditor}
-                                toggleReplyEditor={toggleReplyEditor}
-                            />
+                            <div
+                                className="heex-comment-thread-reply-item"
+                                key={reply.objectId}
+                            >
+                                <div>{reply.username}</div>
+                                <div>{reply.comment}</div>
+                                <div>{reply.createdAt}</div>
+                                <button
+                                    onClick={() =>
+                                        toggleReplyEditor({
+                                            cid: reply.objectId,
+                                            at: reply.username,
+                                        })
+                                    }
+                                >
+                                    Reply
+                                </button>
+                                {renderCommentEditor(
+                                    comment,
+                                    reply,
+                                    reply.objectId
+                                )}
+                            </div>
                         );
                     })}
                 </div>
