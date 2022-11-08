47ae5fa77e1d26859fc271ab479b602829daab0f
Yumin Gui <yumindev@gmail.com>
2022-11-07 20:58:23 -0800

    fix: heex does not work in docusaurus

--- src/Heex/components/_Simple/HeexLogo.jsx
+++ src/Heex/components/_Simple/HeexLogo.jsx
@@ -2,15 +2,15 @@ import React, { useEffect, useState } from "react";
 import { debounce } from "../../utils";
 
 export const HeexLogo = () => {
-    const [width, setWidth] = useState(window.innerWidth);
+    const [width, setWidth] = useState(window?.innerWidth);
 
     useEffect(() => {
-        const onResize = debounce(() => setWidth(window.innerWidth), 100);
+        const onResize = debounce(() => setWidth(window?.innerWidth), 100);
 
-        window.addEventListener("resize", onResize);
+        addEventListener("resize", onResize);
 
         return () => {
-            window.removeEventListener("resize", onResize);
+            removeEventListener("resize", onResize);
         };
     }, []);
 
--- src/Heex/utils/debounce.js
+++ src/Heex/utils/debounce.js
@@ -2,7 +2,7 @@ export const debounce = (fn, interval) => {
     let timerId = null;
 
     return (...args) => {
-        window.clearTimeout(timerId);
+        clearTimeout(timerId);
         timerId = setTimeout(() => {
             fn.apply(null, args);
         }, interval);
